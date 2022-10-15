19b719ba47343bfb9b5076c9c7bbdbaf20589ffa
alexsameri <samerialexx@gmail.com>
2022-10-15 07:31:26 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	
+
 	for (la = 'a'; la <= 'f'; la++)
 		putchar(la);
 
-	putchar ('\n');
+	putchar('\n');
 
 	return (0);
 }
