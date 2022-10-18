6efe537b17aacbd7a9b39ffc090d3069d7d9e839
alexsameri <samerialexx@gmail.com>
2022-10-18 12:12:09 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,8 +9,9 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchaar('t');
+	_putchar('t');
 	_putchar('c');
+	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
