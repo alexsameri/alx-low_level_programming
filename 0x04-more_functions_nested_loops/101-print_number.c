98258d7888e8d9a01e4e0477411530eb25c0fbf1
alexsameri <samerialexx@gmail.com>
2022-10-21 12:09:08 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,38 +6,16 @@
  */
 void print_number(int n)
 {
-	long m;
-	int c;
-	long num;
-
-	num = n;
-
-	if (num < 0)
+	if (n > 0)
 	{
-		num *= -1;
 		_putchar('-');
+		n = -n;
 	}
+	if (n == 0)
+		_putchar('0');
 
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
 }
