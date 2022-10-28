62e70fda448c73d59bb7a74ea5de5220e4ce6efe
alexsameri <samerialexx@gmail.com>
2022-10-28 05:55:32 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a strinf with n
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+}
