4fba59aad4a3ae8c952d9b045c83634d45b7025a
alexsameri <samerialexx@gmail.com>
2022-11-03 11:54:14 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,16 +3,14 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: the string to be printed
- * Return: the length of the string
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*)
+	if (*s != '\0')
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (1 + _strlen_recursions(s + 1));
 	}
-	return (len);
+	return (0);
 }
