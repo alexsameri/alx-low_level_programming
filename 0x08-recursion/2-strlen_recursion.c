59b2c7e31a2b03647a119820dfdabd23994d092f
alexsameri <samerialexx@gmail.com>
2022-11-03 10:18:05 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "man.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to be printed
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursions(s + 1);
+	}
+	return (len);
+}
