ac6269b01c246d0db2db13f4b30f314f8ef72b0d
alexsameri <samerialexx@gmail.com>
2022-11-03 07:16:25 -0700

    Task 0

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -5,6 +5,7 @@
  * _putchar - writes the character c to stdout
  * @c: the character to print
  * Return: On success 1
+ * On error, -1 is returned and error is set appropriatelty
  */
 int _putchar(char c)
 {
