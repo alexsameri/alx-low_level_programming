7066116aea0bfa7eddb5919bea7e73ea6a014e01
alexsameri <samerialexx@gmail.com>
2022-11-04 09:54:30 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,18 +8,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s == '\0')
-	{
-		return (0);
-	}
-
 	if (*s != '\0')
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
-		return (len);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
