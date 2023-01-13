74d0c86e504d2a3d35e8ada9b50b2c1d9cff24a6
alexsameri <samerialexx@gmail.com>
2023-01-13 00:12:41 -0800

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * dlistint_len - counts the number of elements in a linked dlistint_t list
- * @n: the head of the dlistint_t list
+ * @h: the head of the dlistint_t list
  * Return: the number of elements in the dlistint_t list
  */
 size_t dlistint_len(const dlistint_t *h)
