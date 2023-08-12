83b4f9718e8ef7396aa8e5fc7c4a124059b2ebce
alexsameri <samerialexx@gmail.com>
2023-08-12 01:43:16 -0700

    Tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,36 +1,29 @@
-#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
-typedef struct list_t {
-    char *str;
-    struct list_t *next;
-} list_t;
+/**
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
 
-size_t print_list(const list_t *h) {
-    size_t count = 0;
-
-    if (h == NULL) {
-        printf("[0] (nil)\n");
-        return count;
-    }
-
-    printf("[");
-    while (h != NULL) {
-        if (h->str == NULL) {
-            printf("0");
-        } else {
-            printf("%s", h->str);
-        }
-
-        count++;
-        h = h->next;
-
-        // Print comma if there are more elements
-        if (h != NULL) {
-            printf(", ");
-        }
-    }
-    printf("]\n");
-
-    return count;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
