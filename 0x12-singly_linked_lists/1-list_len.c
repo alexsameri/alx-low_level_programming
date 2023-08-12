f82d615e5d546550a4d8422889226a399198380d
alexsameri <samerialexx@gmail.com>
2023-08-12 01:33:49 -0700

    Tasks

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - finds the number of elements
- * in a linked list_t list
- * size_t: size of the list
- * list_t: list
- *
- * @h: the linked list_t list
- * Return: the number of eements in h
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	int count = 0;
 
 	while (h)
 	{
-		elements++;
+		count++;
 		h = h->next;
 	}
-	return (elements);
+	return (count);
 }
